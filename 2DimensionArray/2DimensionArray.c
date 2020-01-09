bbbaeaa36f42bccd95499a78911562f3c35fb8c6
jerrypsm <jerrypsm@gmail.com>
2020-01-09 18:15:32 +0900

    2DimensionArray

---
+++ 2DimensionArray/2DimensionArray.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int main() {
+	int arr[2][3];
+	
+	printf("%p \n", &arr);
+	printf("%p \n", arr);
+	printf("%p\n", arr[0]);
+	printf("%p\n", &arr[0][0]);
+	printf("%p\n", arr[1]);
+	printf("%p\n", &arr[1][0]);
+
+	int arr[2][3] = { {1,2,3}, {4,5,6} };
+	printf("%d \n", sizeof(arr));
+	printf("%d \n", sizeof(arr[0]));
+	return 0;
+}
---
+++ SizeOfArray/SizeOfArray.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main() {
+	int arr[3] = { 1,2,3, };
+	int (*parr)[3] = &arr;
+
+	printf("arr[1] : %d \n", arr[1]);
+	printf("parr[1] : %d \n", (*parr)[1]);
+
+	//test
+	printf("arr : %p \n", arr);
+	printf("arr[0] : %p \n", &arr[0]);
+
+	printf("arr : %p \n", arr);
+	printf("parr : %p \n", parr);
+
+	return 0;
+}
