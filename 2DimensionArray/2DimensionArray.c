6ef15c7cd80a27168a5dbffb6034f96de74c8e5e
박성민 <jerrypsm@gmail.com>
2020-01-10 15:07:04 +0900

    Add test

--- 2DimensionArray/2DimensionArray.c
+++ 2DimensionArray/2DimensionArray.c
@@ -1,7 +1,7 @@
 ﻿#include <stdio.h>
 
 int main() {
-	int arr[2][3];
+	int arr[2][3] = { {1,2,3},{4,5,6} };
 	
 	printf("%p \n", &arr);
 	printf("%p \n", arr);
@@ -13,5 +13,13 @@ int main() {
 	int arr[2][3] = { {1,2,3}, {4,5,6} };
 	printf("%d \n", sizeof(arr));
 	printf("%d \n", sizeof(arr[0]));
+
+	//test
+	printf("test \n");
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 3; j++) {
+			printf("%p \n", &arr[i][j]);
+		}
+	}
 	return 0;
 }
