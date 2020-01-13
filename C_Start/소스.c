0dc53b2878e1df8958fa7c46adba904f558af9c7
박성민 <jerrypsm@gmail.com>
2020-01-13 17:10:34 +0900

    포인터 주소 바꾸기

--- C_Start/소스.c
+++ C_Start/소스.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main() {
-	printf("Github Test - Only English");
+	int a = 5;
+	int b = 10;
+	int* pa = &a;
+	int* pb = &b;
+	printf("*pa : %d / *pb : %d\n", *pa, *pb);
+	int** ppa = &pa;
+	int** ppb = &pb;
+	int* temp = *ppa;
+	*ppa = *ppb;
+	*ppb = temp;
+	printf("*pa : %d / *pb : %d\n", *pa, *pb);
 	return 0;
 }
---
+++ ChangePointer/ChangePointer.c
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+
+int pswap();
+int main() {
+	int a = 5;
+	int b = 10;
+	int* pa = &a;
+	int* pb = &b;
+	printf("*pa : %d / *pb : %d\n", *pa, *pb);
+	printf("&pa : %p / &pb : %p\n", &pa, &pb);
+	/*
+	int** ppa = &pa;
+	int** ppb = &pb;
+	int* temp = *ppa;
+	*ppa = *ppb;
+	*ppb = temp;
+	*/
+	pswap(&pa, &pb);
+	printf("*pa : %d / *pb : %d\n", *pa, *pb);
+	printf("&pa : %p / &pb : %p\n", &pa, &pb);
+	return 0;
+}
+int pswap(int **ppa, int **ppb) {
+	printf("-----pswap 시작-----\n");
+	int* temp = *ppa;
+	*ppa = *ppb;
+	*ppb = temp;
+	printf("-----pswap 종료-----\n");
+	return 0;
+}
