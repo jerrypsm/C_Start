1565127b948b6c4864f2469f47208d8bd6a6d209
박성민 <jerrypsm@gmail.com>
2020-01-13 14:15:24 +0900

    배열을 인자로 받기

---
+++ AddNumber/AddNumber.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+int add_number(int *parr);
+int main() {
+	int arr[5] = { 1,2,3,4,5 };
+	int i;
+	for (i = 0; i < 5; i++) {
+		printf("%d\n", arr[i]);
+	}
+	add_number(arr);
+	for (i = 0; i < 5; i++) {
+		printf("%d\n", arr[i]);
+	}
+	return 0;
+}
+int add_number(int *parr) {
+	int i;
+	for (i = 0; i < 5; i++) {
+		parr[i]++;
+	}
+	return 0;
+}
---
+++ AddNumber2/AddNumber2.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+
+int add_number(int(*parr)[3]);
+int main() {
+	int arr[2][3] = { {1,2,3},{4,5,6} };
+	int i;
+	int j;
+
+	add_number(arr);
+
+	for (i = 0; i < 2; i++) {
+		for (j = 0; j < 3; j++) {
+			printf("%d\n", arr[i][j]);
+		}
+	}
+	return 0;
+}
+int add_number(int(*parr)[3]) {
+	int i;
+	int j;
+	for (i = 0; i < 2; i++) {
+		for (j = 0; j < 3; j++) {
+			parr[i][j]++;
+		}
+	}
+	return 0;
+}
